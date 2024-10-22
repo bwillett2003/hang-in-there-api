bf0207d54e9961c70275b9019d58b24ac57ffb13
Bryan Willett <btwillett2003@gmail.com>
2024-10-22 17:31:44 -0500

    feat: Add endpoint 2 and refactored indentation.

--- app/controllers/api/v1/posters_controller.rb
+++ app/controllers/api/v1/posters_controller.rb
@@ -3,4 +3,15 @@ class Api::V1::PostersController < ApplicationController
     posters = Poster.all
     render json: PosterSerializer.new(posters)
   end
+
+  def show
+    poster = Poster.find(params[:id])
+    render json: PosterSerializer.new(poster)
+  end
+
+  private
+
+  def poster_params
+    params.require(:poster).permit(:name, :description, :price, :year, :vintage, :img_url)
+  end
 end
