66c8f6ac6f9896495c5f385f76e991938334933e
Bryan Willett <btwillett2003@gmail.com>
2024-10-24 14:46:11 -0500

    feat: sorting asc/desc with request_spec testing.

--- app/models/poster.rb
+++ app/models/poster.rb
@@ -3,5 +3,10 @@ class Poster < ApplicationRecord
   validates :price, numericality: { greater_than: 0 }
   validates :year, numericality: { only_integer: true }
   validates :vintage, inclusion: { in: [true, false] }
+
+  def self.filter_by_params(params = {})
+    posters = all
+    posters = posters.order(created_at: (params[:sort] == "desc" ? :desc : :asc)) if params[:sort].present?
+    posters
+  end
 end
-  
